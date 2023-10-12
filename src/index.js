fd0b673ddfc40a37e3e907707757cf341ca5d2bc
YujinB <121761705+YujinB@users.noreply.github.com>
2023-10-12 23:42:47 +0900

    init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './reset.css';
 
 import TopBar from './topBar';
-import Map from './Map';
 import Nav from './Nav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
