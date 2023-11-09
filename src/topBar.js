4d8e5945102428ba548b74058ce40771ef915ac1
Din0lee <96680128+Din0lee@users.noreply.github.com>
2023-11-09 20:47:28 +0900

    Add files via upload

--- src/topBar.js
+++ src/topBar.js
@@ -1,24 +1,20 @@
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import Badge from '@mui/material/Badge';
-import LogoImg from './images/Logo.png';
+
 import './top-bar.css';
+import LogoImg from '../images/Logo.png';
+import AlarmModal from './Alarm_modal';
 
 function TopBar() {
     return (
         <div className="status-bar">
-            <img className="logo" src={LogoImg}></img>
+            <div className="status-bar__column">
+                <img src={LogoImg}
+                    style={{ width: "70px", height: "30px" }} />
+            </div>
             <div className="status-bar__column"></div>
             <div className="status-bar__column">
-                <Badge badgeContent={2} color="error" sx={{ marginRight: "7px" }}>
-                    <NotificationsActiveIcon
-                        size="large"
-                        aria-label="show notifications"
-                        color="inherit"
-                        align-items="center"
 
-                    >
-                    </NotificationsActiveIcon>
-                </Badge>
+                <AlarmModal></AlarmModal>
+
             </div>
         </div>
     );
