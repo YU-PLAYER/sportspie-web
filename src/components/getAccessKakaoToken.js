55c38aff7e333c55fcba25c46cea806f99e0004f
Din0lee <96680128+Din0lee@users.noreply.github.com>
2023-11-18 15:25:12 +0900

    Update getAccessKakaoToken.js
    - qs 라이브러리 사용에 따른 redirect URI 가공
    - header 추가 후 post 수행

--- src/components/getAccessKakaoToken.js
+++ src/components/getAccessKakaoToken.js
@@ -3,7 +3,7 @@ import qs from 'qs';
 
 const REST_API_KEY= process.env.REACT_APP_CLIENT_KAKAO_ID;
 const REACT_APP_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-const REDIRECT_URL = encodeURIComponent(process.env.REACT_APP_NAVER_REDIRECT_URI);
+const REDIRECT_URI = encodeURIComponent(process.env.REACT_APP_NAVER_REDIRECT_URI);
 
 export async function getAccessKakaoToken(authCode) {
     console.log('getAccessKakaoToken called with authCode:', authCode);
@@ -13,7 +13,7 @@ export async function getAccessKakaoToken(authCode) {
           grant_type: 'authorization_code',
           client_id: REST_API_KEY,
           client_secret: REACT_APP_CLIENT_SECRET,
-          redirect_uri: REDIRECT_URL,
+          redirect_uri: REDIRECT_URI,
           code: authCode
       });
 
@@ -27,9 +27,11 @@ export async function getAccessKakaoToken(authCode) {
 
       console.log("%2F 처리" + params_temp);
 
-      const params = params_temp;
+      const body = params_temp;
+
+      const header = {'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'};
         
-      const token_response = await axios.post(apiUrl, params);
+      const token_response = await axios.post(apiUrl,body,header);
       window.Kakao.init(REST_API_KEY);
       console.log('Token response:', token_response);
       const Kakaotoken = token_response.data['access_token'];
