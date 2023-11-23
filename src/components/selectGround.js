cca13dc874715c9601ee6e3ea0dcc32242a93377
Din0lee <act0920@naver.com>
2023-11-23 15:56:13 +0900

    경기글 작성페이지 콤보박스 연동
    - 광역시 / 군,구 선택하여 경기장 선택 가능

--- src/components/selectGround.js
+++ src/components/selectGround.js
@@ -5,35 +5,80 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Stack from '@mui/joy/Stack';
+import { useState } from 'react';
 
 export default function SelectOtherProps() {
 
-/*   const [stadium,setStadium] = useState([
-    '23-11-01 19:00 영남대학교 축구장',
-    '23-11-02 18:00 두류 풋살장',
-    '23-11-03 19:00 강변축구장',
-    '23-11-04 15:00 수성구민운동장',
-    '23-11-05 15:00 월배축구장',
-    '23-11-06 21:00 성서이곡운동장 축구장',
-    '23-11-07 18:00 다사축구장',
-    '23-11-07 20:00 시민운동장 다목적 유소년 축구장',
-    '23-11-07 22:00 성남종합운동장 보조경기장 인조잔디구장'
-  ]); */
+  /*   const [stadium,setStadium] = useState([
+      '23-11-01 19:00 영남대학교 축구장',
+      '23-11-02 18:00 두류 풋살장',
+      '23-11-03 19:00 강변축구장',
+      '23-11-04 15:00 수성구민운동장',
+      '23-11-05 15:00 월배축구장',
+      '23-11-06 21:00 성서이곡운동장 축구장',
+      '23-11-07 18:00 다사축구장',
+      '23-11-07 20:00 시민운동장 다목적 유소년 축구장',
+      '23-11-07 22:00 성남종합운동장 보조경기장 인조잔디구장'
+    ]); */
 
-  function repeatStadium(stadium){
-    let arr=[];
-    for(let i=0; i< stadium.length; i++){
-      arr.push(
-        <MenuItem value={i}>{stadium[i]}</MenuItem>
-      )
+  const cities = [
+    {
+      name: "대구광역시",
+      villages: [
+        {
+          name: "중구",
+          stages: ["중구 1", "중구 2"]
+        },
+        {
+          name: "북구",
+          stages: ["북구 1", "북구 2"]
+        },
+        {
+          name: "동구",
+          stages: ["동구 1", "동구 2"]
+        },
+        {
+          name: "서구",
+          stages: ["서구 1", "서구 2"]
+        },
+        {
+          name: "남구",
+          stages: ["남구 1", "남구 2"]
+        }
+      ]
+    },
+    {
+      name: "경산시",
+      villages: [
+        {
+          name: "대동",
+          stages: ["영남대 1", "영남대 2", "영남대 3"]
+        }
+      ]
     }
-    return arr;
+  ]
+
+  const [city, setCity] = useState("");
+  const [village, setVillage] = useState("");
+  const [villages, setVillages] = useState([]);
+  const [stage, setStage] = useState("");
+  const [stages, setStages] = useState([]);
+  
+
+  const handleCity = (event) => {
+    setCity(event.target.value);
+    setVillages(cities.find(ctr => ctr.name === event.target.value).villages);
+    console.log(cities.find(ctr => ctr.name === event.target.value).villages);
   }
 
-  const [age, setAge] = React.useState('');
+  const handleVillage = (event) => {
+    setVillage(event.target.value);
+    setStages(villages.find(village => village.name === event.target.value).stages);
+    console.log(villages.find(village => village.name === event.target.value).stages);
+  }
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleStage = (event) => {
+    setStage(event.target.value);
   };
 
   return (
@@ -41,55 +86,45 @@ export default function SelectOtherProps() {
       <Stack spacing={2}>
         <d1>
           <FormControl required sx={{ m: 1, minWidth: 120, bgcolor: 'white' }}>
-            <InputLabel id="demo-simple-select-required-label">광역시</InputLabel>
+            <InputLabel>광역시</InputLabel>
             <Select
-              labelId="demo-simple-select-required-label"
-              id="demo-simple-select-required"
-              value={age}
-              label="Age *"
-              onChange={handleChange}
+              value={city}
+              label="광역시 *"
+              onChange={handleCity}
             >
-              <MenuItem value={10}>서울특별시</MenuItem>
-              <MenuItem value={20}>대구광역시</MenuItem>
-              <MenuItem value={30}>경산시 </MenuItem>
+              {cities.map(ctr => (
+                <MenuItem value={ctr.name}>{ctr.name}</MenuItem>
+              ))}
             </Select>
-            {/* <FormHelperText>Required</FormHelperText> */}
           </FormControl>
 
           <FormControl required sx={{ m: 1, minWidth: 120, bgcolor: 'white' }}>
-            <InputLabel id="demo-simple-select-required-label">구·군</InputLabel>
+            <InputLabel>구·군</InputLabel>
             <Select
-              labelId="demo-simple-select-required-label"
-              id="demo-simple-select-required"
-              value={age}
-              label="Age *"
-              onChange={handleChange}
+              value={village}
+              label="구·군 *"
+            onChange={handleVillage}
             >
-              <MenuItem value={10}>북구</MenuItem>
-              <MenuItem value={20}>중구</MenuItem>
-              <MenuItem value={30}>동구</MenuItem>
-              <MenuItem value={40}>서구</MenuItem>
-              <MenuItem value={50}>수성구</MenuItem>
-              <MenuItem value={60}>남구</MenuItem>
-              <MenuItem value={60}>달성군</MenuItem>
-
+              {villages.map(village => (
+                <MenuItem value={village.name}>{village.name}</MenuItem>
+              ))}
             </Select>
-            {/* <FormHelperText>Required</FormHelperText> */}
           </FormControl>
         </d1>
         <d2>
           <FormControl required sx={{ m: 1, minWidth: 255, bgcolor: 'white' }}>
-            <InputLabel id="demo-simple-select-required-label">경기장</InputLabel>
+            <InputLabel>경기장</InputLabel>
             <Select
-              labelId="demo-simple-select-required-label"
-              id="demo-simple-select-required"
-              value={age}
-              label="Age *"
-              onChange={handleChange}
+              value={stage}
+              label="경기장 *"
+              onChange={handleStage}
             >
-              <MenuItem value={10}>경기장1</MenuItem>
+              {stages.map(stage => (
+                <MenuItem value={stage}>{stage}</MenuItem>
+              ))}
+              {/* <MenuItem value={10}>경기장1</MenuItem>
               <MenuItem value={20}>경기장2</MenuItem>
-              <MenuItem value={30}>경기장3fasdfasdfasdfasdfasdfd</MenuItem>
+              <MenuItem value={30}>경기장3fasdfasdfasdfasdfasdfd</MenuItem> */}
             </Select>
             {/* <FormHelperText>Required</FormHelperText> */}
           </FormControl>
